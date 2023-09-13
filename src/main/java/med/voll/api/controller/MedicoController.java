37948537a5e0eef72288fa1a8d37d2f7aa3c4ba7
Guilherme <guilhermebauer16@gmail.com>
2023-09-12 22:29:56 -0300

    feat: Adding method post

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import med.voll.api.medico.DadosCadastroMedicos;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroMedicos dados){
+
+        System.out.println(dados);
+
+    }
+}
